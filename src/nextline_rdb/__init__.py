14a54c37f8f346256bb0f751be06383e2781728e
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-24 15:53:58 -0500

    copy files from nextline-graphql

--- src/nextline_rdb/__init__.py
+++ src/nextline_rdb/__init__.py
@@ -1,3 +1,6 @@
-__all__ = ['Plugin']
+__all__ = ['DB', 'Plugin', 'write_db']
 
+
+from .db import DB
 from .plugin import Plugin
+from .write import write_db
---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import asyncio
+import sys
+import threading
+
+import pytest
+from async_asgi_testclient import TestClient
+
+from nextlinegraphql import create_app
+
+
+@pytest.fixture(autouse=True)
+def recover_trace():
+    """Set the original trace function back after each test"""
+    trace_org = sys.gettrace()
+    yield
+    sys.settrace(trace_org)
+    threading.settrace(trace_org)
+
+
+@pytest.fixture
+async def client(app):
+    async with TestClient(app) as y:
+        await asyncio.sleep(0)
+        yield y
+
+
+@pytest.fixture
+def app():
+    return create_app()
