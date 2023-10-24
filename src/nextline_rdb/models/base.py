872fec47495c91257470427bcddddc25464c1867
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-24 12:00:06 -0400

    Add __repr__() to Base, which can be evaluated to recreate the object.

--- src/nextline_rdb/models/base.py
+++ src/nextline_rdb/models/base.py
@@ -1,5 +1,8 @@
+import enum
+from typing import Any
+
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import ColumnProperty, DeclarativeBase, class_mapper
 
 # Constraints naming conventions
 #
@@ -24,3 +27,31 @@ metadata = MetaData(naming_convention=convention)
 
 class Base(DeclarativeBase):
     metadata = metadata
+
+    def __repr__(self) -> str:
+        '''Return a string that can be used to reconstruct the object.'''
+        props = class_mapper(self.__class__).iterate_properties
+        columns = (p for p in props if isinstance(p, ColumnProperty))
+        keys = (c.key for c in columns if self._repr_filter(c.key))
+        fmts = (f'{k}={self._repr_val(k)}' for k in keys)
+        return f'{self.__class__.__qualname__}({", ".join(fmts)})'
+
+    def _repr_filter(self, key: str) -> bool:
+        '''Exclude if the value is None. Override this method if necessary.'''
+        val = getattr(self, key)
+        return val is not None
+
+    def _repr_val(self, key: str) -> str:
+        '''Format the value so it can be reconstructed. Override this method if necessary.'''
+        val = getattr(self, key)
+        return repr_val(val)
+
+
+def repr_val(val: Any) -> str:
+    match val:
+        case enum.Enum():
+            return str(val)
+        case int():
+            return f'{val:_}'
+        case _:
+            return repr(val)
