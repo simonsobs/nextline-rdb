d70083e55d670c33166b00c1859865260fe644fe
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-26 11:05:21 -0400

    add test_run.py

--- tests/models/test_run.py
+++ tests/models/test_run.py
@@ -1,26 +1,43 @@
 import datetime
 
+from hypothesis import given
+from hypothesis import strategies as st
 from sqlalchemy import select
 
-from nextline_rdb import DB
 from nextline_rdb.models import Run
 
+from .funcs import DB
 
-def test_one():
-    db = DB()
-    run_no = 1
-    with db.session() as session:
-        model = Run(
-            run_no=run_no,
-            state="running",
-            started_at=datetime.datetime.utcnow(),
-            ended_at=datetime.datetime.utcnow(),
-            script="pass",
-        )
-        session.add(model)
-        session.commit()
-        assert model.id
 
-    with db.session() as session:
-        stmt = select(Run).filter_by(run_no=run_no)
-        model = session.execute(stmt).scalar_one()
+@st.composite
+def st_run(draw: st.DrawFn):
+    run_no = draw(st.integers(min_value=1, max_value=2_147_483_647))
+    state = draw(st.one_of(st.none(), st.text()))
+    started_at = draw(st.one_of(st.none(), st.datetimes()))
+    ended_at = draw(st.one_of(st.none(), st.datetimes()))
+    script = draw(st.one_of(st.none(), st.text()))
+    exception = draw(st.one_of(st.none(), st.text()))
+    model = Run(
+        run_no=run_no,
+        state=state,
+        started_at=started_at,
+        ended_at=ended_at,
+        script=script,
+        exception=exception,
+    )
+    return model
+
+
+@given(st.data())
+async def test_repr(data: st.DataObject):
+    async with DB() as Session:
+        async with (Session() as session, session.begin()):
+            model = data.draw(st_run())
+            session.add(model)
+
+        async with Session() as session:
+            rows = await session.scalars(select(Run))
+            for row in rows:
+                repr_ = repr(row)
+                assert Run, datetime
+                assert repr_ == repr(eval(repr_))
