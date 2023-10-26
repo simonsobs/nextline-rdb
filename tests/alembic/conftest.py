ff404d3e66069278f70881b086b652780c2b69d5
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-26 16:47:43 -0400

    Update the fixture in_alembic_dir()

--- tests/alembic/conftest.py
+++ tests/alembic/conftest.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 import pytest
 
+import nextline_rdb
+
 
 @pytest.fixture
-def in_alembic_dir(monkeypatch: pytest.MonkeyPatch):
-    import nextline_rdb
-
+def in_alembic_dir(monkeypatch: pytest.MonkeyPatch) -> Path:
+    '''Move to the directory with alembic.ini'''
     path = Path(nextline_rdb.__file__).parent
-
+    assert (path / 'alembic.ini').is_file()
     monkeypatch.chdir(path)
+    return path
