de4556d0759d42d4b71ffc470f79504c1942dc25
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-26 17:02:53 -0400

    Update test_run_check.py

--- tests/alembic/test_run_check.py
+++ tests/alembic/test_run_check.py
@@ -1,16 +1,19 @@
-'''Run alembic check
-
-- https://github.com/sqlalchemy/alembic/releases/tag/rel_1_9_0
-- https://github.com/sqlalchemy/alembic/issues/724
-- https://github.com/sqlalchemy/alembic/pull/1101
-'''
 from pathlib import Path
 
 from alembic import command
 from alembic.config import Config
 
 
-def test_run(in_alembic_dir):
+def test_migration_up_to_date(in_alembic_dir: Path) -> None:
+    '''Run alembic check
+
+    This test fails if the head of the migration versions is not consistent of the ORM
+    models defined in src/nextline_rdb/models.
+
+    - https://github.com/sqlalchemy/alembic/releases/tag/rel_1_9_0
+    - https://github.com/sqlalchemy/alembic/issues/724
+    - https://github.com/sqlalchemy/alembic/pull/1101
+    '''
     del in_alembic_dir
     config = Config(Path.cwd() / 'alembic.ini')
     command.upgrade(config, "head")
