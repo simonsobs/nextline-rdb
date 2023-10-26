9462195f7224e28fee7f604955cebeec28685d01
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-26 14:41:19 -0400

    add st_model_prompt() and test_prompt.py

--- src/nextline_rdb/tests/strategies/models/__init__.py
+++ src/nextline_rdb/tests/strategies/models/__init__.py
@@ -1,4 +1,9 @@
-__all__ = ['st_model_run', 'st_model_trace']
+__all__ = [
+    'st_model_prompt',
+    'st_model_run',
+    'st_model_trace',
+]
 
+from .prompt import st_model_prompt
 from .run import st_model_run
 from .trace import st_model_trace
---
+++ tests/models/test_prompt.py
@@ -0,0 +1,30 @@
+import datetime
+
+from hypothesis import given
+from hypothesis import strategies as st
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+from nextline_rdb.models import Prompt
+from nextline_rdb.tests.strategies.models import st_model_prompt
+
+from .funcs import DB
+
+
+@given(st.data())
+async def test_repr(data: st.DataObject):
+    async with DB() as Session:
+        async with (Session() as session, session.begin()):
+            model = data.draw(st_model_prompt())
+            session.add(model)
+
+        async with Session() as session:
+            rows = await session.scalars(
+                select(Prompt).options(
+                    selectinload(Prompt.run), selectinload(Prompt.trace)
+                )
+            )
+            for row in rows:
+                repr_ = repr(row)
+                assert Prompt, datetime
+                assert repr_ == repr(eval(repr_))
