a4d07ddce9677f1920ee5ce94cf2c4a256a14568
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-27 16:28:12 -0400

    Rewrite in_alembic_dir() with ALEMBIC_INI

--- tests/alembic/conftest.py
+++ tests/alembic/conftest.py
@@ -2,13 +2,12 @@ from pathlib import Path
 
 import pytest
 
-import nextline_rdb
+from nextline_rdb.db import ALEMBIC_INI
 
 
 @pytest.fixture
 def in_alembic_dir(monkeypatch: pytest.MonkeyPatch) -> Path:
     '''Move to the directory with alembic.ini'''
-    path = Path(nextline_rdb.__file__).parent
-    assert (path / 'alembic.ini').is_file()
+    path = Path(ALEMBIC_INI).parent
     monkeypatch.chdir(path)
     return path
