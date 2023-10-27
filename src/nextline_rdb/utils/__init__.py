a1db7579fee1eab73c2cff16e8ffb24ccad81856
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-27 15:51:39 -0400

    Add ensure_async_url() and ensure_sync_url()

--- src/nextline_rdb/utils/__init__.py
+++ src/nextline_rdb/utils/__init__.py
@@ -1,6 +1,9 @@
 __all__ = [
+    'ensure_async_url',
+    'ensure_sync_url',
     'is_timezone_aware',
     'utc_timestamp',
 ]
 
+from .url import ensure_async_url, ensure_sync_url
 from .utc import is_timezone_aware, utc_timestamp
