7b1379495c5ddbfde87f2ece7aba79dab96fc109
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-01 13:36:27 -0400

    Replace args of st_model_run()

--- src/nextline_rdb/models/strategies/run.py
+++ src/nextline_rdb/models/strategies/run.py
@@ -5,31 +5,57 @@ from hypothesis import strategies as st
 
 from nextline_rdb.models import Run
 
-from .utils import st_datetimes
-
-MAX_INT = 2_147_483_647
+from .utils import st_datetimes, st_none_or, st_sqlite_ints
 
 
 @st.composite
 def st_model_run(
     draw: st.DrawFn,
-    prev: Optional[Run] = None,
-    time: Optional[dt.datetime] = None,
-) -> Run | None:
-    if prev and prev.run_no >= MAX_INT:
-        return None
-    min_run_no = prev.run_no + 1 if prev else 1
-    min_started_at = time + dt.timedelta(seconds=1) if time else dt.datetime.min
-    run_no = draw(st.integers(min_value=min_run_no, max_value=MAX_INT))
-    state = draw(st.one_of(st.none(), st.text()))
-    started_at = draw(st.one_of(st.none(), st_datetimes(min_value=min_started_at)))
+    min_run_no: Optional[int] = None,
+    max_run_no: Optional[int] = None,
+    min_started_at: Optional[dt.datetime] = None,
+    max_started_at: Optional[dt.datetime] = None,
+    min_ended_at: Optional[dt.datetime] = None,
+    max_ended_at: Optional[dt.datetime] = None,
+) -> Run:
+    # Validate options
+    if min_run_no is None:
+        min_run_no = 1
+
+    min_started_at = min_started_at or dt.datetime.min
+    max_started_at = max_started_at or dt.datetime.max
+    min_ended_at = min_ended_at or dt.datetime.min
+    max_ended_at = max_ended_at or dt.datetime.max
+    assert min_started_at <= max_started_at
+    assert min_started_at <= max_ended_at
+    max_started_at = min(max_started_at, max_ended_at)
+    min_ended_at = max(min_started_at, min_ended_at)
+    assert min_ended_at <= max_ended_at
+
+    # Generate model arguments
+    run_no = draw(st_sqlite_ints(min_value=min_run_no, max_value=max_run_no))
+
+    state = draw(st_none_or(st.text()))
+
+    started_at = draw(
+        st_none_or(st_datetimes(min_value=min_started_at, max_value=max_started_at))
+    )
+    if started_at:
+        min_ended_at = max(started_at, min_ended_at)
+    assert min_ended_at <= max_ended_at
     ended_at = (
         None
         if started_at is None
-        else draw(st.one_of(st.none(), st_datetimes(min_value=started_at)))
+        else draw(
+            st_none_or(st_datetimes(min_value=min_ended_at, max_value=max_ended_at))
+        )
     )
-    script = draw(st.one_of(st.none(), st.text()))
-    exception = draw(st.one_of(st.none(), st.text()))
+
+    script = draw(st_none_or(st.text()))
+
+    exception = draw(st_none_or(st.text()))
+
+    # Create model
     model = Run(
         run_no=run_no,
         state=state,
@@ -44,25 +70,28 @@ def st_model_run(
 @st.composite
 def st_model_run_list(
     draw: st.DrawFn,
+    min_size: int = 0,
     max_size: Optional[int] = None,
-):
+) -> list[Run]:
+    run_nos = draw(
+        st.lists(
+            st_sqlite_ints(min_value=1),
+            min_size=min_size,
+            max_size=max_size,
+            unique=True,
+        ).map(sorted)
+    )
     runs = list[Run]()
-    prev: Run | None = None
-    time: dt.datetime | None = None
-    n_runs = 0
-    while True:
-        if max_size is not None and n_runs >= max_size:
-            break
-        if draw(st.booleans()):
-            break
-        run = draw(st_model_run(prev=prev, time=time))
-        if run is None:
-            break
+    min_started_at = draw(st_datetimes())
+    for run_no in run_nos:
+        run = draw(
+            st_model_run(
+                min_run_no=run_no, max_run_no=run_no, min_started_at=min_started_at
+            )
+        )
         if run.started_at is not None:
-            time = run.started_at
+            min_started_at = run.started_at + dt.timedelta(seconds=1)
         if run.ended_at is not None:
-            time = run.ended_at
+            min_started_at = run.ended_at + dt.timedelta(seconds=1)
         runs.append(run)
-        n_runs += 1
-        prev = run
     return runs
--- tests/models/strategies/test_run.py
+++ tests/models/strategies/test_run.py
@@ -3,24 +3,84 @@ from hypothesis import strategies as st
 from sqlalchemy import select
 
 from nextline_rdb.models import Run
-from nextline_rdb.models.strategies import st_model_run, st_model_run_list
+from nextline_rdb.models.strategies import (
+    SQLITE_INT_MAX,
+    st_datetimes,
+    st_model_run,
+    st_model_run_list,
+    st_none_or,
+    st_sqlite_ints,
+)
 
 from ...db import AsyncDB
 
 
 @given(st.data())
 async def test_st_model_run(data: st.DataObject) -> None:
-    time = data.draw(st.one_of(st.none(), st.datetimes()))
+    min_run_no = data.draw(st_none_or(st_sqlite_ints(min_value=1)), label='min_run_no')
 
-    run = data.draw(st_model_run(time=time))
-    assert run
+    max_run_no = data.draw(
+        st_none_or(st_sqlite_ints(min_value=min_run_no or 1)), label='max_run_no'
+    )
 
-    if time and run.started_at:
-        assert time < run.started_at
-    if time and run.ended_at:
-        assert time < run.ended_at
+    min_started_at = data.draw(st_none_or(st_datetimes()), label='min_started_at')
+
+    max_started_at = data.draw(
+        st_none_or(st_datetimes(min_value=min_started_at)), label='max_started_at'
+    )
+
+    min_ended_at = data.draw(
+        st_none_or(st_datetimes(min_value=min_started_at)), label='min_ended_at'
+    )
+
+    min_max_ended_at = (
+        max(min_started_at, min_ended_at)
+        if min_started_at and min_ended_at
+        else (min_started_at or min_ended_at)
+    )
+
+    max_ended_at = data.draw(
+        st_none_or(st_datetimes(min_value=min_max_ended_at)), label='max_ended_at'
+    )
+
+    run = data.draw(
+        st_model_run(
+            min_run_no=min_run_no,
+            max_run_no=max_run_no,
+            min_started_at=min_started_at,
+            max_started_at=max_started_at,
+            min_ended_at=min_ended_at,
+            max_ended_at=max_ended_at,
+        )
+    )
+
+    if min_run_no is None:
+        assert 1 <= run.run_no
+    else:
+        assert min_run_no <= run.run_no
+
+    if max_run_no is None:
+        assert run.run_no <= SQLITE_INT_MAX
+    else:
+        assert run.run_no <= max_run_no
+
+    if min_started_at and run.started_at:
+        assert min_started_at <= run.started_at
+
+    if max_started_at and run.started_at:
+        assert run.started_at <= max_started_at
+
+    if min_ended_at and run.ended_at:
+        assert min_ended_at <= run.ended_at
+
+    if max_ended_at and run.ended_at:
+        assert run.ended_at <= max_ended_at
+
     if run.started_at and run.ended_at:
-        assert run.started_at < run.ended_at
+        assert run.started_at <= run.ended_at
+
+    if not run.started_at:
+        assert not run.ended_at
 
     async with AsyncDB() as db:
         async with db.session.begin() as session:
@@ -34,9 +94,9 @@ async def test_st_model_run(data: st.DataObject) -> None:
 
 @given(st.data())
 async def test_st_model_run_lists(data: st.DataObject) -> None:
-    max_n_runs = data.draw(st.integers(min_value=0, max_value=10))
-    runs = data.draw(st_model_run_list(max_size=max_n_runs))
-    assert len(runs) <= max_n_runs
+    max_size = data.draw(st.integers(min_value=0, max_value=10))
+    runs = data.draw(st_model_run_list(max_size=max_size))
+    assert len(runs) <= max_size
     run_nos = [run.run_no for run in runs]
     assert run_nos == sorted(run_nos)
 
