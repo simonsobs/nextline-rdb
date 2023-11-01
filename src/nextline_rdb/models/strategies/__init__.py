65d5be871fd6d6798e4b3f820db71052de14eb0d
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-01 10:43:27 -0400

    Add st_sqlite_ints()

--- src/nextline_rdb/models/strategies/__init__.py
+++ src/nextline_rdb/models/strategies/__init__.py
@@ -7,10 +7,11 @@ __all__ = [
     'SQLITE_INT_MAX',
     'SQLITE_INT_MIN',
     'st_datetimes',
+    'st_sqlite_ints',
 ]
 
 from .prompt import st_model_prompt
 from .run import st_model_run, st_model_run_list
 from .stdout import st_model_stdout
 from .trace import st_model_trace
-from .utils import SQLITE_INT_MAX, SQLITE_INT_MIN, st_datetimes
+from .utils import SQLITE_INT_MAX, SQLITE_INT_MIN, st_datetimes, st_sqlite_ints
--- src/nextline_rdb/models/strategies/utils.py
+++ src/nextline_rdb/models/strategies/utils.py
@@ -7,6 +7,32 @@ SQLITE_INT_MIN = -9_223_372_036_854_775_808
 SQLITE_INT_MAX = 9_223_372_036_854_775_807
 
 
+def st_sqlite_ints(
+    min_value: Optional[int] = None,
+    max_value: Optional[int] = None,
+) -> st.SearchStrategy[int]:
+    '''A strategy for integers within the range of SQLite's `INTEGER` type.
+
+    >>> int_ = st_sqlite_ints().example()
+    >>> int_ >= SQLITE_INT_MIN
+    True
+
+    >>> int_ <= SQLITE_INT_MAX
+    True
+    '''
+    if min_value is None:
+        min_value = SQLITE_INT_MIN
+    else:
+        min_value = max(min_value, SQLITE_INT_MIN)
+
+    if max_value is None:
+        max_value = SQLITE_INT_MAX
+    else:
+        max_value = min(max_value, SQLITE_INT_MAX)
+
+    return st.integers(min_value=min_value, max_value=max_value)
+
+
 def st_datetimes(
     min_value: Optional[dt.datetime] = None,
     max_value: Optional[dt.datetime] = None,
