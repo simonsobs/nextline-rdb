9b4f44348f99b8e52a25c1a21c9602ce61e1d028
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-02 16:35:13 -0400

    Copy st_min_max_tuples() from c5backup

--- src/nextline_rdb/utils/strategies/__init__.py
+++ src/nextline_rdb/utils/strategies/__init__.py
@@ -3,10 +3,17 @@ __all__ = [
     'st_datetimes',
     'SQLITE_INT_MAX',
     'SQLITE_INT_MIN',
+    'st_min_max_tuples',
     'st_none_or',
     'st_sqlite_ints',
 ]
 
 
 from .datetime import st_datetime_ranges, st_datetimes
-from .misc import SQLITE_INT_MAX, SQLITE_INT_MIN, st_none_or, st_sqlite_ints
+from .misc import (
+    SQLITE_INT_MAX,
+    SQLITE_INT_MIN,
+    st_min_max_tuples,
+    st_none_or,
+    st_sqlite_ints,
+)
--- src/nextline_rdb/utils/strategies/misc.py
+++ src/nextline_rdb/utils/strategies/misc.py
@@ -42,3 +42,46 @@ def st_sqlite_ints(
         max_value = min(max_value, SQLITE_INT_MAX)
 
     return st.integers(min_value=min_value, max_value=max_value)
+
+
+@st.composite
+def st_min_max_tuples(
+    draw,
+    min_min=0,
+    max_min: Optional[int] = None,
+    min_max=0,
+    max_max: Optional[int] = None,
+):
+    '''Generate 2-tuples (min, max) in which min <= max or max is None.
+
+    >>> min_, max_ = st_min_max_tuples(max_max=10).example()
+
+    The results can be, for example, used as min_value and max_value of st.integers().
+
+    >>> i = st.integers(min_value=min_, max_value=max_).example()
+    >>> min_ <= i <= max_
+    True
+
+    The results can also be used to generate lists of a certain length.
+
+    >>> l = st.lists(st.integers(), min_size=min_, max_size=max_).example()
+    >>> min_ <= len(l) <= max_
+    True
+    '''
+
+    if max_max is not None:
+        if max_min is not None:
+            assert max_min <= max_max
+        else:
+            max_min = max_max
+
+    min_ = draw(st.integers(min_value=min_min, max_value=max_min))
+
+    min_max = max(min_max, min_)
+
+    if max_max is None:
+        max_ = draw(st.one_of(st.none(), st.integers(min_value=min_max)))
+    else:
+        max_ = draw(st.integers(min_value=min_max, max_value=max_max))
+
+    return min_, max_
