c33ed0336feb8bd123cd4631e1101e717b6780e2
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-03 16:42:45 -0400

    Rewrite st_sqlite_ints with safe_compare()

--- tests/utils/strategies/test_st_sqlite_ints.py
+++ tests/utils/strategies/test_st_sqlite_ints.py
@@ -3,6 +3,7 @@ from typing import TypeVar
 from hypothesis import given
 from hypothesis import strategies as st
 
+from nextline_rdb.utils import safe_compare
 from nextline_rdb.utils.strategies import (
     SQLITE_INT_MAX,
     SQLITE_INT_MIN,
@@ -22,9 +23,4 @@ def test_st_sqlite_ints(data: st.DataObject) -> None:
     i = data.draw(st_sqlite_ints(min_value=min_, max_value=max_))
 
     assert SQLITE_INT_MIN <= i <= SQLITE_INT_MAX
-
-    if min_ is not None:
-        assert min_ <= i
-
-    if max_ is not None:
-        assert i <= max_
+    assert safe_compare(min_) <= i <= safe_compare(max_)
