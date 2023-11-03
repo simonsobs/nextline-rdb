3f5261f319e8a663d1606fe431a58d950d1563c0
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-03 13:55:01 -0400

    Update the docstring of st_started_at_ended_at()

--- src/nextline_rdb/models/strategies/utils.py
+++ src/nextline_rdb/models/strategies/utils.py
@@ -12,9 +12,17 @@ def st_started_at_ended_at(
     min_end: Optional[dt.datetime] = None,
     max_end: Optional[dt.datetime] = None,
 ) -> st.SearchStrategy[tuple[None, None] | tuple[dt.datetime, Optional[dt.datetime]]]:
-    '''
+    '''Generate two naive datetime objects: started_at and ended_at.
+
+    `started_at` and `ended_at` can be `None`. If `started_at` is `None`,
+    `ended_at` is always `None`.
+
+    When neither of them is `None`, `started_at` is earlier than or the same
+    time as `ended_at`.
+
     >>> st_started_at_ended_at().example()
     (...)
+
     '''
     return st_ranges(
         st_=st_datetimes(),
