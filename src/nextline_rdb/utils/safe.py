13325012c9dd69ae396deae1bd0ad7b0c6888731
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-03 16:02:27 -0400

    Add docstrings to safe_min() and safe_max()

--- src/nextline_rdb/utils/safe.py
+++ src/nextline_rdb/utils/safe.py
@@ -5,8 +5,46 @@ T = TypeVar('T')
 
 
 def safe_min(vals: Iterable[T], default: Optional[T] = None) -> Optional[T]:
+    '''The smallest item in `vals` that is not `None`.
+    
+    >>> safe_min([None, 1, 2, None])
+    1
+    
+    It returns `None` if `vals` is empty or all items in `vals` are `None`.
+    >>> print(safe_min([None, None]))
+    None
+
+    >>> print(safe_min([]))
+    None
+
+    If `default` is given, it returns `default` instead of `None`.
+    >>> safe_min([None, None], default=-1)
+    -1
+    
+    >>> safe_min([], default=-1)
+    -1
+    '''
     return min((v for v in vals if v is not None), default=default)
 
 
 def safe_max(vals: Iterable[T], default: Optional[T] = None) -> Optional[T]:
+    '''The largest item in `vals` that is not `None`.
+    
+    >>> safe_max([None, 1, 2, None])
+    2
+
+    It returns `None` if `vals` is empty or all items in `vals` are `None`.
+    >>> print(safe_max([None, None]))
+    None
+
+    >>> print(safe_max([]))
+    None
+
+    If `default` is given, it returns `default` instead of `None`.
+    >>> safe_max([None, None], default=-1)
+    -1
+
+    >>> safe_max([], default=-1)
+    -1
+    '''
     return max((v for v in vals if v is not None), default=default)
