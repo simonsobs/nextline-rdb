3a38fb759208d3aa4e3ac93cdd43501e6ca02754
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-09 11:59:38 -0500

    Update st_model_run() and add st_model_stdout_list()

--- src/nextline_rdb/models/strategies/__init__.py
+++ src/nextline_rdb/models/strategies/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     'st_model_run',
     'st_model_run_list',
     'st_model_stdout',
+    'st_model_stdout_list',
     'st_model_trace',
     'st_model_trace_list',
     'st_thread_task_no',
@@ -12,6 +13,6 @@ __all__ = [
 
 from .prompt import st_model_prompt, st_model_prompt_list
 from .run import st_model_run, st_model_run_list
-from .stdout import st_model_stdout
+from .stdout import st_model_stdout, st_model_stdout_list
 from .trace import st_model_trace, st_model_trace_list, st_thread_task_no
 from .utils import st_started_at_ended_at
