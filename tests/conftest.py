0ac45a784b735684293a349d519f3a268ac9c6ed
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-14 16:53:03 -0500

    Remove the fixture recover_trace()
    The script is now run in a spawned process

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,8 @@
 import asyncio
-import sys
-import threading
 
 import pytest
 from async_asgi_testclient import TestClient
 from nextlinegraphql import create_app
-
-
-@pytest.fixture(autouse=True)
-def recover_trace():
-    """Set the original trace function back after each test"""
-    trace_org = sys.gettrace()
-    yield
-    sys.settrace(trace_org)
-    threading.settrace(trace_org)
 
 
 @pytest.fixture
