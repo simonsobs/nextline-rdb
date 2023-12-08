84246ed2695322cef2d676b7f330ebfb18f88b33
Tai Sakuma <tai.sakuma@gmail.com>
2023-12-08 15:14:14 -0500

    Clean code in plugin.py

--- src/nextline_rdb/plugin.py
+++ src/nextline_rdb/plugin.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 from apluggy import asynccontextmanager
 from dynaconf import Dynaconf, Validator
+from nextline import Nextline
 from nextlinegraphql.hook import spec
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
@@ -39,27 +40,38 @@ class Plugin:
 
     @spec.hookimpl
     def configure(self, settings: Dynaconf) -> None:
-        self._db = DB(settings.db['url'])
-
-    def _initial_run_no(self) -> Optional[int]:
-        with self._db.session() as session:
-            last_run = self._last_run(session)
-            if last_run is None:
-                return None
-            else:
-                return last_run.run_no + 1
-
-    def _initial_script(self) -> Optional[str]:
-        with self._db.session() as session:
-            last_run = self._last_run(session)
-            if last_run is None:
-                return None
-            else:
-                return last_run.script
+        self._url = settings.db['url']
 
     @spec.hookimpl
     def schema(self) -> tuple[type, type | None, type | None]:
         return (Query, Mutation, Subscription)
+
+    @spec.hookimpl
+    @asynccontextmanager
+    async def lifespan(self, context: Mapping) -> AsyncIterator[None]:
+        nextline = context['nextline']
+        self._db = DB(self._url)
+        with self._db:
+            await self._initialize_nextline(nextline)
+            async with write_db(nextline, self._db):
+                yield
+
+    async def _initialize_nextline(self, nextline: Nextline) -> None:
+        run_no, script = self._last_run_no_and_script()
+        if run_no is not None:
+            run_no += 1
+            if run_no >= nextline._init_options.run_no_start_from:
+                nextline._init_options.run_no_start_from = run_no
+        if script is not None:
+            nextline._init_options.statement = script
+
+    def _last_run_no_and_script(self) -> tuple[Optional[int], Optional[str]]:
+        with self._db.session() as session:
+            last_run = self._last_run(session)
+            if last_run is None:
+                return None, None
+            else:
+                return last_run.run_no, last_run.script
 
     def _last_run(self, session: Session) -> Optional[models.Run]:
         stmt = select(models.Run, func.max(models.Run.run_no))
@@ -70,22 +82,6 @@ class Plugin:
             msg = "No previous runs were found in the DB"
             logger.info(msg)
             return None
-
-    @spec.hookimpl
-    @asynccontextmanager
-    async def lifespan(self, context: Mapping) -> AsyncIterator[None]:
-        nextline = context['nextline']
-        with self._db:
-            run_no = self._initial_run_no()
-            if run_no is not None:
-                nextline._init_options.run_no_start_from = max(
-                    run_no, nextline._init_options.run_no_start_from
-                )
-            nextline._init_options.statement = (
-                self._initial_script() or nextline._init_options.statement
-            )
-            async with write_db(nextline, self._db):
-                yield
 
     @spec.hookimpl
     def update_strawberry_context(self, context: MutableMapping) -> None:
