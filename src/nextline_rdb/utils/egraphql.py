028f24195b31a9c2cfdc2843f265c83922b80f8a
Tai Sakuma <tai.sakuma@gmail.com>
2023-12-19 18:47:54 -0500

    Add docstring to EGraphQL

--- src/nextline_rdb/utils/egraphql.py
+++ src/nextline_rdb/utils/egraphql.py
@@ -9,7 +9,26 @@ if TYPE_CHECKING:
 class EGraphQL(GraphQL):
     '''Extend the strawberry GraphQL app to override the `get_context` method
 
+    This class is implemented in the way described in the strawberry document:
     https://strawberry.rocks/docs/integrations/asgi
+
+    This class is used in tests.
+
+    Example:
+
+    >>> import strawberry
+
+    >>> from nextline_rdb import AsyncDB
+    >>> from nextline_rdb.schema import Query
+    >>> from nextline_rdb.utils import EGraphQL
+
+    >>> async def main():
+    ...    async with AsyncDB() as db:
+    ...        schema = strawberry.Schema(query=Query)
+    ...        app = EGraphQL(schema).set_db(db)
+
+    >>> import asyncio
+    >>> asyncio.run(main())
     '''
 
     def set_db(self, db: 'AsyncDB') -> 'EGraphQL':
