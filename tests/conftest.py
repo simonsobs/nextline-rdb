d7900f361fc862cdd6f63731ffeef8d2d74d3fea
Tai Sakuma <tai.sakuma@gmail.com>
2023-12-20 13:13:49 -0500

    Move fixtures from conftest.py to test_plugin.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,7 @@
-import asyncio
 import os
 
-import pytest
-from async_asgi_testclient import TestClient
 from hypothesis import settings
-from nextlinegraphql import create_app
 
 if os.getenv('GITHUB_ACTIONS') == 'true':
     settings.register_profile('ci', deadline=None)
     settings.load_profile('ci')
-
-
-@pytest.fixture
-async def client(app):
-    async with TestClient(app) as y:
-        await asyncio.sleep(0)
-        yield y
-
-
-@pytest.fixture
-def app():
-    return create_app()
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,8 @@
 import asyncio
 
+import pytest
 from async_asgi_testclient import TestClient
+from nextlinegraphql import create_app
 from nextlinegraphql.plugins.ctrl.test import run_statement
 from nextlinegraphql.plugins.graphql.test import gql_request
 
@@ -14,3 +16,15 @@ async def test_one(client: TestClient):
     runs = data['history']['runs']
     edges = runs['edges']
     assert 2 == len(edges)
+
+
+@pytest.fixture
+async def client(app):
+    async with TestClient(app) as y:
+        await asyncio.sleep(0)
+        yield y
+
+
+@pytest.fixture
+def app():
+    return create_app()
