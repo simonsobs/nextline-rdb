632a1121d4b8914ecec702aaa14b7e46fb88c922
Tai Sakuma <tai.sakuma@gmail.com>
2024-01-10 11:19:14 -0500

    Extract lifespan() from Plugin as a function

--- src/nextline_rdb/plugin.py
+++ src/nextline_rdb/plugin.py
@@ -40,7 +40,8 @@ class Plugin:
 
     @spec.hookimpl
     def configure(self, settings: Dynaconf) -> None:
-        self._url = settings.db['url']
+        url = settings.db['url']
+        self._db = AsyncDB(url)
 
     @spec.hookimpl
     def schema(self) -> tuple[type, type | None, type | None]:
@@ -50,39 +51,47 @@ class Plugin:
     @asynccontextmanager
     async def lifespan(self, context: Mapping) -> AsyncIterator[None]:
         nextline = context['nextline']
-        self._db = AsyncDB(self._url)
-        async with self._db:
-            await self._initialize_nextline(nextline)
-            async with async_write_db(nextline, self._db):
-                yield
-
-    async def _initialize_nextline(self, nextline: Nextline) -> None:
-        run_no, script = await self._last_run_no_and_script()
-        if run_no is not None:
-            run_no += 1
-            if run_no >= nextline._init_options.run_no_start_from:
-                nextline._init_options.run_no_start_from = run_no
-        if script is not None:
-            nextline._init_options.statement = script
-
-    async def _last_run_no_and_script(self) -> tuple[Optional[int], Optional[str]]:
-        async with self._db.session() as session:
-            last_run = await self._last_run(session)
-            if last_run is None:
-                return None, None
-            else:
-                return last_run.run_no, last_run.script
-
-    async def _last_run(self, session: AsyncSession) -> Optional[models.Run]:
-        stmt = select(models.Run, func.max(models.Run.run_no))
-        if model := (await session.execute(stmt)).scalar_one_or_none():
-            return model
-        else:
-            logger = getLogger(__name__)
-            msg = "No previous runs were found in the DB"
-            logger.info(msg)
-            return None
+        async with lifespan(nextline, self._db):
+            yield
 
     @spec.hookimpl
     def update_strawberry_context(self, context: MutableMapping) -> None:
         context['db'] = self._db
+
+
+@asynccontextmanager
+async def lifespan(nextline: Nextline, db: AsyncDB) -> AsyncIterator[None]:
+    async with db:
+        await _initialize_nextline(nextline, db)
+        async with async_write_db(nextline, db):
+            yield
+
+
+async def _initialize_nextline(nextline: Nextline, db: AsyncDB) -> None:
+    run_no, script = await _last_run_no_and_script(db)
+    if run_no is not None:
+        run_no += 1
+        if run_no >= nextline._init_options.run_no_start_from:
+            nextline._init_options.run_no_start_from = run_no
+    if script is not None:
+        nextline._init_options.statement = script
+
+
+async def _last_run_no_and_script(db: AsyncDB) -> tuple[Optional[int], Optional[str]]:
+    async with db.session() as session:
+        last_run = await _last_run(session)
+        if last_run is None:
+            return None, None
+        else:
+            return last_run.run_no, last_run.script
+
+
+async def _last_run(session: AsyncSession) -> Optional[models.Run]:
+    stmt = select(models.Run, func.max(models.Run.run_no))
+    if model := (await session.execute(stmt)).scalar_one_or_none():
+        return model
+    else:
+        logger = getLogger(__name__)
+        msg = "No previous runs were found in the DB"
+        logger.info(msg)
+        return None
