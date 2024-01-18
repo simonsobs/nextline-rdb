3855990df7fc21ce1297276e87f7e91ff0b1b3e9
Tai Sakuma <tai.sakuma@gmail.com>
2024-01-18 10:37:19 -0500

    Copy the GraphQL "history" to "rdb"

--- tests/schema/graphql/__init__.py
+++ tests/schema/graphql/__init__.py
@@ -3,9 +3,9 @@ from pathlib import Path
 pwd = Path(__file__).resolve().parent
 
 sub = pwd / 'queries'
-QUERY_HISTORY = (sub / 'History.gql').read_text()
-QUERY_HISTORY_RUN = (sub / 'HistoryRun.gql').read_text()
-QUERY_HISTORY_RUNS = (sub / 'HistoryRuns.gql').read_text()
+QUERY_RDB_CONNECTIONS = (sub / 'RDBConnections.gql').read_text()
+QUERY_RDB_RUNS = (sub / 'RDBRuns.gql').read_text()
+QUERY_RDB_RUN = (sub / 'RDBRun.gql').read_text()
 
 sub = pwd / 'mutations'
 MUTATE_RDB_DELETE_RUNS = (sub / 'RDBDeleteRuns.gql').read_text()
--- tests/schema/queries/test_rdb.py
+++ tests/schema/queries/test_rdb.py
@@ -6,7 +6,7 @@ from nextline_rdb.models import Run
 from nextline_rdb.models.strategies import st_model_run_list
 from nextline_rdb.schema import Query
 
-from ..graphql import QUERY_HISTORY
+from ..graphql import QUERY_RDB_CONNECTIONS
 
 
 @given(runs=st_model_run_list(generate_traces=True, min_size=0, max_size=3))
@@ -17,11 +17,11 @@ async def test_history(runs: list[Run]) -> None:
         async with db.session.begin() as session:
             session.add_all(runs)
 
-        resp = await schema.execute(QUERY_HISTORY, context_value={'db': db})
+        resp = await schema.execute(QUERY_RDB_CONNECTIONS, context_value={'db': db})
 
     assert resp.data
-    history = resp.data['history']
-    runs_ = history['runs']['edges']
+    rdb = resp.data['rdb']
+    runs_ = rdb['runs']['edges']
 
     assert len(runs) == len(runs_)
 
