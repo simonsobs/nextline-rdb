696e4a9599d32587568dbde809a3e6bd9269257f
Tai Sakuma <tai.sakuma@gmail.com>
2024-01-25 11:58:48 -0500

    Add WriteTraceTable

--- src/nextline_rdb/write/__init__.py
+++ src/nextline_rdb/write/__init__.py
@@ -5,7 +5,9 @@ from nextline import Nextline
 from nextline_rdb.db import DB
 
 from .write_run_table import WriteRunTable
+from .write_trace_table import WriteTraceTable
 
 
 def register(nextline: Nextline, db: DB) -> None:
     nextline.register(WriteRunTable(db=db))
+    nextline.register(WriteTraceTable(db=db))
