21a5a86b701717f3ed1cf39c1eae8004943c9a16
Tai Sakuma <tai.sakuma@gmail.com>
2024-02-06 16:47:49 -0500

    Update a test

--- src/nextline_rdb/models/strategies/tests/test_st_script.py
+++ src/nextline_rdb/models/strategies/tests/test_st_script.py
@@ -1,9 +1,8 @@
 from hypothesis import given
 from hypothesis import strategies as st
-from sqlalchemy import select
-from sqlalchemy.orm import selectinload
 
 from nextline_rdb.db import DB
+from nextline_rdb.utils import class_name_and_primary_keys_of, load_all
 from nextline_rdb.utils.strategies import st_none_or
 
 from ... import Model, Script
@@ -24,8 +23,13 @@ async def test_db(script: Script) -> None:
     async with DB(use_migration=False, model_base_class=Model) as db:
         async with db.session.begin() as session:
             session.add(script)
-        async with db.session() as session:
-            select_script = select(Script).options(selectinload(Script._current))
-            script_ = await session.scalar(select_script)
+            added = list(session.new)
+            assert script in added
+        added = sorted(added, key=class_name_and_primary_keys_of)
+        repr_added = [repr(m) for m in added]
 
-    assert repr(script) == repr(script_)
+        async with db.session() as session:
+            loaded = await load_all(session, Model)
+            repr_loaded = [repr(m) for m in loaded]
+
+    assert repr_added == repr_loaded
