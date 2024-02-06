b62b85bab0c78a462b21d0a3a516eaaf2933a61a
Tai Sakuma <tai.sakuma@gmail.com>
2024-02-05 19:05:33 -0500

    Add test_model_script.py

---
+++ src/nextline_rdb/models/tests/test_model_script.py
@@ -0,0 +1,34 @@
+from hypothesis import given
+from sqlalchemy import select
+
+from nextline_rdb.db import DB
+from nextline_rdb.utils import (
+    all_declared_models_based_on,
+    class_name_and_primary_keys_of,
+)
+
+from .. import CurrentScript, Model, Script
+from ..strategies import st_model_script
+
+
+@given(script=st_model_script())
+async def test_repr(script: Script) -> None:
+    async with DB(use_migration=False, model_base_class=Model) as db:
+        async with db.session.begin() as session:
+            session.add(script)
+            added = list(session.new)
+        added = sorted(added, key=class_name_and_primary_keys_of)
+        repr_added = repr(added)
+
+        async with db.session() as session:
+            loaded = [
+                m
+                for c in all_declared_models_based_on(Model)
+                for m in (await session.scalars(select(c))).all()
+            ]
+            loaded = sorted(loaded, key=class_name_and_primary_keys_of)
+            repr_loaded = repr(loaded)
+
+            assert repr_added == repr_loaded
+            assert CurrentScript  # type: ignore[truthy-function]
+            assert repr_loaded == repr(eval(repr_loaded))
