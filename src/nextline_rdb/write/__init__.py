18f8b0306ba385753b257f09aaecadb3e37a21c2
Tai Sakuma <tai.sakuma@gmail.com>
2024-02-07 14:59:03 -0500

    Insert a new run on_start_run() instead of on_initialize_run().

--- src/nextline_rdb/write/__init__.py
+++ src/nextline_rdb/write/__init__.py
@@ -6,12 +6,14 @@ from nextline_rdb.db import DB
 
 from .write_prompt_table import WritePromptTable
 from .write_run_table import WriteRunTable
+from .write_script_table import WriteScriptTable
 from .write_stdout_table import WriteStdoutTable
 from .write_trace_table import WriteTraceTable
 
 
 def register(nextline: Nextline, db: DB) -> None:
     nextline.register(WriteRunTable(db=db))
+    nextline.register(WriteScriptTable(db=db))
     nextline.register(WriteTraceTable(db=db))
     nextline.register(WritePromptTable(db=db))
     nextline.register(WriteStdoutTable(db=db))
