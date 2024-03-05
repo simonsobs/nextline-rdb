1d82458326c08b11159778f4d48c10d73d1e0f94
Tai Sakuma <tai.sakuma@gmail.com>
2024-03-05 11:19:24 -0500

    Replace lists with connections

--- src/nextline_rdb/schema/nodes/trace_node.py
+++ src/nextline_rdb/schema/nodes/trace_node.py
@@ -1,14 +1,75 @@
 import datetime
-from typing import TYPE_CHECKING, Annotated, Optional
+from typing import TYPE_CHECKING, Annotated, Optional, cast
 
 import strawberry
+from sqlalchemy import select
+from strawberry.types import Info
 
 from nextline_rdb import models as db_models
+from nextline_rdb.db import DB
+from nextline_rdb.models import Prompt, Stdout
+from nextline_rdb.pagination import SortField
+
+from ..pagination import Connection, load_connection
 
 if TYPE_CHECKING:
     from .prompt_node import PromptNode
     from .run_node import RunNode
     from .stdout_node import StdoutNode
+
+
+async def _resolve_prompts(
+    info: Info,
+    root: 'TraceNode',
+    before: Optional[str] = None,
+    after: Optional[str] = None,
+    first: Optional[int] = None,
+    last: Optional[int] = None,
+) -> 'Connection[PromptNode]':
+    from .prompt_node import PromptNode
+
+    sort = [SortField('prompt_no')]
+    select_model = select(Prompt).where(Prompt.trace_id == root._model.id)
+    db = cast(DB, info.context['db'])
+    async with db.session() as session:
+        return await load_connection(
+            session,
+            Prompt,
+            create_node_from_model=PromptNode.from_model,
+            select_model=select_model,
+            sort=sort,
+            before=before,
+            after=after,
+            first=first,
+            last=last,
+        )
+
+
+async def _resolve_stdouts(
+    info: Info,
+    root: 'TraceNode',
+    before: Optional[str] = None,
+    after: Optional[str] = None,
+    first: Optional[int] = None,
+    last: Optional[int] = None,
+) -> 'Connection[StdoutNode]':
+    from .stdout_node import StdoutNode
+
+    sort = [SortField('written_at')]
+    select_model = select(Stdout).where(Stdout.trace_id == root._model.id)
+    db = cast(DB, info.context['db'])
+    async with db.session() as session:
+        return await load_connection(
+            session,
+            Stdout,
+            create_node_from_model=StdoutNode.from_model,
+            select_model=select_model,
+            sort=sort,
+            before=before,
+            after=after,
+            first=first,
+            last=last,
+        )
 
 
 @strawberry.type
@@ -29,25 +90,13 @@ class TraceNode:
 
         return RunNode.from_model(self._model.run)
 
-    # prompts: Connection[PromptHistory] = strawberry.field(
-    #     resolver=query_connection_prompt
-    # )
+    prompts: Connection[
+        Annotated['PromptNode', strawberry.lazy('.prompt_node')]
+    ] = strawberry.field(resolver=_resolve_prompts)
 
-    # stdouts: Connection[StdoutHistory] = strawberry.field(
-    #     resolver=query_connection_stdout
-    # )
-
-    @strawberry.field
-    def prompts(self) -> list[Annotated['PromptNode', strawberry.lazy('.prompt_node')]]:
-        from .prompt_node import PromptNode
-
-        return [PromptNode.from_model(m) for m in self._model.prompts]  # type: ignore
-
-    @strawberry.field
-    def stdouts(self) -> list[Annotated['StdoutNode', strawberry.lazy('.stdout_node')]]:
-        from .stdout_node import StdoutNode
-
-        return [StdoutNode.from_model(m) for m in self._model.stdouts]  # type: ignore
+    stdouts: Connection[
+        Annotated['StdoutNode', strawberry.lazy('.stdout_node')]
+    ] = strawberry.field(resolver=_resolve_stdouts)
 
     @classmethod
     def from_model(cls: type['TraceNode'], model: db_models.Trace):
