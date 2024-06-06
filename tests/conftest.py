217c6abbd84ab4963d5decd2110f9e338cf06baa
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-06 11:01:27 -0400

    Remove the deadline from Hypothesis settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,4 @@
-import os
-
 from hypothesis import settings
 
-if os.getenv('GITHUB_ACTIONS') == 'true':
-    settings.register_profile('ci', deadline=None)
-    settings.load_profile('ci')
+settings.register_profile('no-deadline', deadline=None)
+settings.load_profile('no-deadline')
