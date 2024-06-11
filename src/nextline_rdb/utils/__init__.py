eaa5ad8a87328f64979468c1baa55ad5c03ae026
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-11 17:32:37 -0400

    Add `until_not_none()`

--- src/nextline_rdb/utils/__init__.py
+++ src/nextline_rdb/utils/__init__.py
@@ -7,6 +7,8 @@ __all__ = [
     'safe_compare',
     'safe_max',
     'safe_min',
+    'until_not_none',
+    'UntilNotNoneTimeout',
     'ensure_async_url',
     'ensure_sync_url',
     'is_timezone_aware',
@@ -21,5 +23,6 @@ from .sa import (
     primary_keys_of,
 )
 from .safe import safe_compare, safe_max, safe_min
+from .until import UntilNotNoneTimeout, until_not_none
 from .url import ensure_async_url, ensure_sync_url
 from .utc import is_timezone_aware, utc_timestamp
---
+++ tests/utils/test_until.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+from nextline.utils import aiterable
+
+from nextline_rdb.utils import UntilNotNoneTimeout, until_not_none
+
+
+@given(st.data())
+async def test_return(data: st.DataObject) -> None:
+    val = data.draw(st.text())
+    nones = data.draw(st.lists(st.none(), max_size=10))
+    ret = nones + [val]
+    func = aiterable(ret).__anext__
+    assert (await until_not_none(func)) == val
+
+
+async def test_timeout() -> None:
+    async def gen_none():
+        while True:
+            await asyncio.sleep(0)
+            yield None
+
+    g = gen_none()
+    func = g.__anext__
+    with pytest.raises(UntilNotNoneTimeout):
+        await until_not_none(func, timeout=0.001)
+
+
+@pytest.mark.timeout(5)
+async def test_timeout_never_return() -> None:
+    async def func():
+        while True:
+            await asyncio.sleep(0)
+
+    with pytest.raises(UntilNotNoneTimeout):
+        await until_not_none(func, timeout=0.001)
