ccd5393410df8e04ed2562b694dd8e0c4c906f97
Tai Sakuma <tai.sakuma@gmail.com>
2024-12-04 17:28:50 -0500

    Update `test_st_script_list.py`

--- src/nextline_rdb/models/strategies/tests/test_st_script_list.py
+++ src/nextline_rdb/models/strategies/tests/test_st_script_list.py
@@ -1,48 +1,67 @@
-from hypothesis import given
+from typing import Optional, TypedDict
+
+from hypothesis import Phase, given, note, settings
 from hypothesis import strategies as st
 
-from nextline_rdb.db import DB
-from nextline_rdb.utils import class_name_and_primary_keys_of, load_all
+from nextline_test_utils import safe_compare as sc
 from nextline_test_utils.strategies import st_ranges
 
-from ... import Model, Script
+from ... import Model
 from .. import st_model_script_list
+from .funcs import assert_model_persistence
 
 
+class StModelScriptListKwargs(TypedDict, total=False):
+    min_size: int
+    max_size: Optional[int]
+
+
+@st.composite
+def st_st_model_script_list_kwargs(draw: st.DrawFn) -> StModelScriptListKwargs:
+    kwargs = StModelScriptListKwargs()
+
+    if draw(st.booleans()):
+        min_size, max_size = draw(
+            st_ranges(
+                st.integers,
+                min_start=0,
+                max_end=4,
+                allow_start_none=False,
+                allow_end_none=False,
+            )
+        )
+        assert isinstance(min_size, int)
+        kwargs['min_size'] = min_size
+        kwargs['max_size'] = max_size
+
+    return kwargs
+
+
+@given(kwargs=st_st_model_script_list_kwargs())
+def test_st_model_script_list_kwargs(kwargs: StModelScriptListKwargs) -> None:
+    assert sc(kwargs.get('min_size')) <= sc(kwargs.get('max_size'))
+
+
+@settings(phases=(Phase.generate,))  # Avoid shrinking
 @given(st.data())
 async def test_options(data: st.DataObject) -> None:
-    min_size, max_size = data.draw(
-        st_ranges(
-            st.integers,
-            min_start=0,
-            max_end=5,
-            allow_start_none=False,
-            allow_end_none=False,
-        )
-    )
-    assert isinstance(min_size, int)
-    assert isinstance(max_size, int)
+    # Generate options of the strategy to be tested
+    kwargs = data.draw(st_st_model_script_list_kwargs())
+    note(kwargs)
 
-    scripts = data.draw(st_model_script_list(min_size=min_size, max_size=max_size))
+    # Call the strategy to be tested
+    scripts = data.draw(st_model_script_list(**kwargs))
 
-    assert min_size <= len(scripts) <= max_size
+    # Assert the generated values
+    min_size = kwargs.get('min_size', 0)
+    max_size = kwargs.get('max_size')
 
-    current_script = {script for script in scripts if script.current}
-    assert len(current_script) <= 1
+    assert sc(min_size) <= len(scripts) <= sc(max_size)
+
+    current_scripts = {script for script in scripts if script.current}
+    assert len(current_scripts) <= 1
 
 
-@given(scripts=st_model_script_list(min_size=0, max_size=3))
-async def test_db(scripts: list[Script]) -> None:
-    async with DB(use_migration=False, model_base_class=Model) as db:
-        async with db.session.begin() as session:
-            session.add_all(scripts)
-            added = list(session.new)
-            assert set(scripts) <= set(added)
-        added = sorted(added, key=class_name_and_primary_keys_of)
-        repr_added = [repr(m) for m in added]
-
-        async with db.session() as session:
-            loaded = await load_all(session, Model)
-            repr_loaded = [repr(m) for m in loaded]
-
-    assert repr_added == repr_loaded
+@given(instances=st_model_script_list(min_size=0, max_size=3))
+async def test_db(instances: list[Model]) -> None:
+    await assert_model_persistence(instances)
