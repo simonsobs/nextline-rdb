306fb8a64864b0003c0a787e1d881d61a2274df2
Tai Sakuma <tai.sakuma@gmail.com>
2024-12-04 16:23:19 -0500

    Update `test_st_prompt.py`

--- src/nextline_rdb/models/strategies/tests/test_st_prompt.py
+++ src/nextline_rdb/models/strategies/tests/test_st_prompt.py
@@ -1,30 +1,74 @@
-from hypothesis import given
+from typing import Optional, TypedDict
+
+from hypothesis import Phase, given, settings
 from hypothesis import strategies as st
-from sqlalchemy import select
-from sqlalchemy.orm import selectinload
 
 from nextline_rdb.db import DB
+from nextline_rdb.utils import class_name_and_primary_keys_of, load_all
+from nextline_test_utils.strategies import st_graphql_ints, st_none_or
 
-from ... import Model, Prompt
-from .. import st_model_prompt
+from ... import Model, TraceCall
+from .. import st_model_prompt, st_model_trace_call
 
 
-@given(st.data())
-async def test_st_model_prompt(data: st.DataObject) -> None:
-    prompt = data.draw(st_model_prompt())
+class StModelPromptKwargs(TypedDict, total=False):
+    prompt_no: Optional[int]
+    trace_call: Optional[TraceCall]
 
+
+@st.composite
+def st_st_model_prompt_kwargs(draw: st.DrawFn) -> StModelPromptKwargs:
+    kwargs = StModelPromptKwargs()
+
+    if draw(st.booleans()):
+        kwargs['prompt_no'] = draw(st_none_or(st_graphql_ints(min_value=1)))
+
+    if draw(st.booleans()):
+        kwargs['trace_call'] = draw(
+            st_none_or(st_model_trace_call(generate_prompts=False))
+        )
+
+    return kwargs
+
+
+@given(kwargs=st_st_model_prompt_kwargs())
+def test_st_model_prompt_kwargs(kwargs: StModelPromptKwargs) -> None:
+    del kwargs
+
+
+@settings(phases=(Phase.generate,))  # Avoid shrinking
+@given(data=st.data())
+def test_options(data: st.DataObject) -> None:
+    # Generate options of the strategy to be tested
+    kwargs = data.draw(st_st_model_prompt_kwargs())
+
+    # Call the strategy to be tested
+    prompt = data.draw(st_model_prompt(**kwargs))
+
+    # Assert the generated values
+    prompt_no = kwargs.get('prompt_no')
+    trace_call = kwargs.get('trace_call')
+
+    if prompt_no is not None:
+        assert prompt.prompt_no == prompt_no
+
+    if trace_call is not None:
+        assert prompt.trace_call == trace_call
+
+
+@settings(phases=(Phase.generate,))  # Avoid shrinking
+@given(instance=st_model_prompt())
+async def test_db(instance: Model) -> None:
     async with DB(use_migration=False, model_base_class=Model) as db:
         async with db.session.begin() as session:
-            session.add(prompt)
+            session.add(instance)
+            added = list(session.new)
+            assert instance in added
+        added = sorted(added, key=class_name_and_primary_keys_of)
+        repr_added = [repr(m) for m in added]
+
         async with db.session() as session:
-            select_prompt = select(Prompt)
-            prompt_ = await session.scalar(
-                select_prompt.options(
-                    selectinload(Prompt.run),
-                    selectinload(Prompt.trace),
-                    selectinload(Prompt.trace_call),
-                )
-            )
-            assert prompt_
-            session.expunge_all()
-    assert repr(prompt) == repr(prompt_)
+            loaded = await load_all(session, Model)
+            repr_loaded = [repr(m) for m in loaded]
+
+    assert repr_added == repr_loaded
