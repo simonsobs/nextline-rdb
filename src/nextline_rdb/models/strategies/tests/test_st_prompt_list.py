a83c539b355ed0656655b45cf0952abfe9282abd
Tai Sakuma <tai.sakuma@gmail.com>
2024-12-04 16:03:14 -0500

    Update `test_st_prompt_list.py`

--- src/nextline_rdb/models/strategies/tests/test_st_prompt_list.py
+++ src/nextline_rdb/models/strategies/tests/test_st_prompt_list.py
@@ -1,12 +1,14 @@
-from hypothesis import Phase, given, settings
+from typing import Optional, TypedDict
+
+from hypothesis import Phase, given, note, settings
 from hypothesis import strategies as st
-from sqlalchemy import select
 
 from nextline_rdb.db import DB
-from nextline_rdb.utils import class_name_and_primary_keys_of
-from nextline_test_utils.strategies import st_none_or
+from nextline_rdb.utils import class_name_and_primary_keys_of, load_all
+from nextline_test_utils import safe_compare as sc
+from nextline_test_utils.strategies import st_none_or, st_ranges
 
-from ... import Model, Prompt
+from ... import Model, Run
 from .. import (
     st_model_prompt_list,
     st_model_run,
@@ -15,38 +17,87 @@ from .. import (
 )
 
 
-@settings(phases=(Phase.generate,))  # Avoid shrinking
+class StModelPromptListKwargs(TypedDict, total=False):
+    run: Optional[Run]
+    min_size: int
+    max_size: Optional[int]
+
+
+@st.composite
+def st_st_model_prompt_list_kwargs(draw: st.DrawFn) -> StModelPromptListKwargs:
+    kwargs = StModelPromptListKwargs()
+
+    if draw(st.booleans()):
+        # generate_traces=False because True would generate a trace with prompts
+        run = draw(st_none_or(st_model_run(generate_traces=False)))
+        kwargs['run'] = run
+        if run:
+            draw(st_none_or(st_model_trace_list(run=run, min_size=0, max_size=3)))
+            draw(st_none_or(st_model_trace_call_list(run=run, min_size=0, max_size=5)))
+
+    max_size: int | None = None
+    if draw(st.booleans()) or 1:
+        min_size, max_size = draw(
+            st_ranges(
+                st.integers,
+                min_start=0,
+                max_end=4,
+                allow_start_none=False,
+                allow_end_none=False,
+            )
+        )
+        assert isinstance(min_size, int)
+        kwargs['min_size'] = min_size
+        kwargs['max_size'] = max_size
+
+    return kwargs
+
+
+@given(kwargs=st_st_model_prompt_list_kwargs())
+def test_st_model_prompt_list_kwargs(kwargs: StModelPromptListKwargs) -> None:
+    assert sc(kwargs.get('min_size')) <= sc(kwargs.get('max_size'))
+
+
+@settings(max_examples=500, phases=(Phase.generate,))  # Avoid shrinking
 @given(st.data())
-async def test_st_model_prompt_lists(data: st.DataObject) -> None:
-    # generate_traces=False because True would generate a trace with prompts
-    if run := data.draw(st_none_or(st_model_run(generate_traces=False))):
-        data.draw(st_none_or(st_model_trace_list(run=run, min_size=0, max_size=3)))
-        data.draw(st_none_or(st_model_trace_call_list(run=run, min_size=0, max_size=5)))
+async def test_options(data: st.DataObject) -> None:
+    # Generate options of the strategy to be tested
+    kwargs = data.draw(st_st_model_prompt_list_kwargs())
+    note(kwargs)
 
-    max_size = data.draw(st.integers(min_value=0, max_value=5))
+    # Call the strategy to be tested
+    prompts = data.draw(st_model_prompt_list(**kwargs))
 
-    prompts = data.draw(st_model_prompt_list(run=run, max_size=max_size))
+    # Assert the generated values
+    run = kwargs.get('run')
+    min_size = kwargs.get('min_size', 0)
+    max_size = kwargs.get('max_size')
 
-    assert len(prompts) <= max_size
+    if run and not run.trace_calls:
+        # `prompts` is not generated if `run` with no `trace_calls` is provided
+        assert not prompts
+    else:
+        assert min_size <= len(prompts) <= sc(max_size)
 
     if prompts:
         runs = set(prompt.trace.run for prompt in prompts)
         assert len(runs) == 1
         assert run is None or run is runs.pop()
 
+
+@settings(phases=(Phase.generate,))  # Avoid shrinking
+@given(instances=st_model_prompt_list(max_size=5))
+async def test_db(instances: list[Model]) -> None:
     async with DB(use_migration=False, model_base_class=Model) as db:
         async with db.session.begin() as session:
-            session.add_all(prompts)
-
-        prompts = sorted(prompts, key=class_name_and_primary_keys_of)
-        expected = repr(prompts)
+            session.add_all(instances)
+            added = list(session.new)
+            assert set(instances) <= set(added)
+        added = sorted(added, key=class_name_and_primary_keys_of)
+        repr_added = [repr(m) for m in added]
 
         async with db.session() as session:
-            select_prompt = select(Prompt)
-            prompts_ = (await session.scalars(select_prompt)).all()
-            session.expunge_all()
+            loaded = await load_all(session, Model)
+            repr_loaded = [repr(m) for m in loaded]
 
-        prompts_ = sorted(prompts_, key=class_name_and_primary_keys_of)
-        actual = repr(prompts_)
-
-    assert expected == actual
+    assert repr_added == repr_loaded
