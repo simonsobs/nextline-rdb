07bb2f47baedfa24a6547c2d9dcdf7ce110ddf27
Tai Sakuma <tai.sakuma@gmail.com>
2024-12-04 19:07:50 -0500

    Update `test_st_trace_call_list.py`

--- src/nextline_rdb/models/strategies/tests/test_st_trace_call_list.py
+++ src/nextline_rdb/models/strategies/tests/test_st_trace_call_list.py
@@ -1,47 +1,82 @@
+from typing import Optional, TypedDict
+
 from hypothesis import Phase, given, settings
 from hypothesis import strategies as st
-from sqlalchemy import select
 
-from nextline_rdb.db import DB
-from nextline_rdb.utils import class_name_and_primary_keys_of
-from nextline_test_utils.strategies import st_none_or
+from nextline_test_utils import safe_compare as sc
+from nextline_test_utils.strategies import st_none_or, st_ranges
 
-from ... import Model, TraceCall
+from ... import Run, TraceCall
 from .. import st_model_run, st_model_trace_call_list, st_model_trace_list
+from .funcs import assert_model_persistence
 
 
-@settings(phases=(Phase.generate,))  # Avoid shrinking
+class StModelTraceCallListKwargs(TypedDict, total=False):
+    run: Optional[Run]
+    min_size: int
+    max_size: Optional[int]
+
+
+@st.composite
+def st_st_model_trace_call_list_kwargs(draw: st.DrawFn) -> StModelTraceCallListKwargs:
+    kwargs = StModelTraceCallListKwargs()
+
+    if draw(st.booleans()):
+        # generate_traces=False because True would generate a trace with trace_calls
+        run = draw(st_none_or(st_model_run(generate_traces=False)))
+        kwargs['run'] = run
+        if run:
+            draw(st_none_or(st_model_trace_list(run=run, max_size=3)))
+
+    if draw(st.booleans()):
+        min_size, max_size = draw(
+            st_ranges(
+                st.integers,
+                min_start=0,
+                max_end=4,
+                allow_start_none=False,
+                allow_end_none=False,
+            )
+        )
+        assert isinstance(min_size, int)
+        kwargs['min_size'] = min_size
+        kwargs['max_size'] = max_size
+
+    return kwargs
+
+
+@given(kwargs=st_st_model_trace_call_list_kwargs())
+def test_st_model_trace_call_list_kwargs(kwargs: StModelTraceCallListKwargs) -> None:
+    assert sc(kwargs.get('min_size')) <= sc(kwargs.get('max_size'))
+
+
+@settings(max_examples=200, phases=(Phase.generate,))  # Avoid shrinking
 @given(st.data())
-async def test_st_model_trace_call_lists(data: st.DataObject) -> None:
-    # generate_traces=False because True would generate a trace with trace_calls
-    if run := data.draw(st_none_or(st_model_run(generate_traces=False))):
-        data.draw(st_none_or(st_model_trace_list(run=run, min_size=0, max_size=3)))
-        assert run.traces is not None
+async def test_options(data: st.DataObject) -> None:
+    # Generate options of the strategy to be tested
+    kwargs = data.draw(st_st_model_trace_call_list_kwargs())
 
-    max_size = data.draw(st.integers(min_value=0, max_value=5))
+    # Call the strategy to be tested
+    trace_calls = data.draw(st_model_trace_call_list(**kwargs))
 
-    trace_calls = data.draw(st_model_trace_call_list(run=run, max_size=max_size))
+    # Assert the generated values
+    run = kwargs.get('run')
+    min_size = kwargs.get('min_size', 0)
+    max_size = kwargs.get('max_size')
 
-    assert len(trace_calls) <= max_size
+    if run and not run.traces:
+        # `trace_calls` is not generated if `run` with no `traces` is provided
+        assert not trace_calls
+    else:
+        assert min_size <= len(trace_calls) <= sc(max_size)
 
     if trace_calls:
         runs = set(trace_call.trace.run for trace_call in trace_calls)
         assert len(runs) == 1
         assert run is None or run is runs.pop()
 
-    async with DB(use_migration=False, model_base_class=Model) as db:
-        async with db.session.begin() as session:
-            session.add_all(trace_calls)
 
-        trace_calls = sorted(trace_calls, key=class_name_and_primary_keys_of)
-        expected = repr(trace_calls)
-
-        async with db.session() as session:
-            select_trace_call = select(TraceCall)
-            trace_calls_ = (await session.scalars(select_trace_call)).all()
-            session.expunge_all()
-
-        trace_calls_ = sorted(trace_calls_, key=class_name_and_primary_keys_of)
-        actual = repr(trace_calls_)
-
-    assert expected == actual
+@settings(phases=(Phase.generate,))  # Avoid shrinking
+@given(instances=st_model_trace_call_list(max_size=5))
+async def test_db(instances: list[TraceCall]) -> None:
+    await assert_model_persistence(instances)
