a434079f6314714de8cfb19f2431008c66cb5121
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-20 14:06:44 -0500

    Reduce sample sizes in Hypothesis tests

---
+++ src/nextline_rdb/alembic/models/conftest.py
@@ -0,0 +1,3 @@
+from hypothesis import settings
+
+settings.register_profile('reduced-size', max_examples=10)
---
+++ src/nextline_rdb/alembic/models/rev_f9a742bb2297/conftest.py
@@ -0,0 +1,3 @@
+from hypothesis import settings
+
+settings.load_profile('reduced-size')
