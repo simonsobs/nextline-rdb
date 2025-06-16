a528c200546e6329208656387567bc46b443de8b
Tai Sakuma <tai.sakuma@gmail.com>
2025-06-16 13:57:26 -0400

    Move `st_enum_type()` to `utils`

---
+++ src/nextline_rdb/utils/strategies.py
@@ -0,0 +1,27 @@
+import keyword
+from enum import Enum
+from string import ascii_lowercase, ascii_uppercase
+
+from hypothesis import strategies as st
+
+
+@st.composite
+def st_enum_type(draw: st.DrawFn) -> type[Enum]:
+    '''Generate an Enum type.
+
+    >>> enum_type = st_enum_type().example()
+    >>> list(enum_type)
+    [...]
+
+    Code based on:
+    https://github.com/HypothesisWorks/hypothesis/issues/2693#issuecomment-823710924
+
+    '''
+    names_ = st.text(ascii_lowercase, min_size=1)
+    names = (
+        st.builds(lambda x: x.capitalize(), names_)
+        .filter(str.isidentifier)
+        .filter(lambda x: not keyword.iskeyword(x))
+    )
+    values = st.lists(st.text(ascii_uppercase, min_size=1), min_size=1, unique=True)
+    return draw(st.builds(Enum, names, values))  # type: ignore
